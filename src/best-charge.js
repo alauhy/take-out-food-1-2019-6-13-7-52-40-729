6fc289acbba6b8f088ef4b3c0147698bfc248ee7
lauhiuyee <lauhiuyee@lauhiuyeede-MacBook-Pro.local>
2019-07-10 22:42:38 +0800

    new file:   spec/total-price-no-promotion-spec.js 	modified:   src/best-charge.js

--- src/best-charge.js
+++ src/best-charge.js
@@ -7,10 +7,25 @@ let isHalfPromotion =(goodsCode)=>{
   return getPromotion[1].items.indexOf(goodsCode)!== -1;
 }
 
+let getTotalPriceNoPromotion = (inputs) =>{
+  let cnt=0;
+  for(var i in inputs){
+    for(var j in items){
+      let arr=inputs[i].split(' x ');
+      if(arr[0]===items[j].id){
+        cnt+=items[j].price*parseFloat(arr[1]);
+      }
+
+    }
+
+  }
+  return cnt;
+}
 
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
 module.exports = {
   isHalfPromotion,
+  getTotalPriceNoPromotion,
 };
